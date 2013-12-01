8dd93d45dac2d0589dc2a48a705453f731042d33
Robert Coelho <me@iscoelho.com>
2013-12-01 08:14:58 -0800

    Fix UUID reflection - hackish things must be done for the sake of humanity

--- src/main/java/lilypad/bukkit/connect/ConnectPluginListener.java
+++ src/main/java/lilypad/bukkit/connect/ConnectPluginListener.java
@@ -57,7 +57,7 @@ public class ConnectPluginListener implements Listener {
 			Method getHandleMethod = player.getClass().getMethod("getHandle");
 			Object entityPlayer = getHandleMethod.invoke(player);
 			if (playerData[3].length() == 32) {
-				ReflectionUtils.setFinalField(entityPlayer.getClass(), entityPlayer, "uniqueID", UUID.fromString(playerData[3].substring(0, 8) + "-" + playerData[3].substring(8, 12) + "-" + playerData[3].substring(12, 16) + "-" + playerData[3].substring(16, 20) + "-" + playerData[3].substring(20, 32)));
+				ReflectionUtils.setFinalField(entityPlayer.getClass().getSuperclass().getSuperclass().getSuperclass(), entityPlayer, "uniqueID", UUID.fromString(playerData[3].substring(0, 8) + "-" + playerData[3].substring(8, 12) + "-" + playerData[3].substring(12, 16) + "-" + playerData[3].substring(16, 20) + "-" + playerData[3].substring(20, 32)));
 			} else {
 				System.out.println("[Connect] Unexpected UUID length: " + playerData[3].length());
 			}
