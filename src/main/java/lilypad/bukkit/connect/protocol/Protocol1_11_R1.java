74f16a7cc96168c5a7ca42a14f08bb3bc9a763f6
Robert Coelho <me@iscoelho.com>
2016-11-17 14:47:34 -0800

    1.11 support

---
+++ src/main/java/lilypad/bukkit/connect/protocol/Protocol1_11_R1.java
@@ -0,0 +1,40 @@
+package lilypad.bukkit.connect.protocol;
+
+public class Protocol1_11_R1 implements IProtocol {
+
+	@Override
+	public String getGeneralVersion() {
+		return "1.11";
+	}
+	
+	@Override
+	public String getNettyInjectorChannelFutureList() {
+		return "g";
+	}
+
+	@Override
+	public String getOfflineInjectorServerConnection() {
+		return "f";
+	}
+
+	@Override
+	public String getPacketInjectorProtocolDirections() {
+		return "h";
+	}
+
+	@Override
+	public String getPacketInjectorDecodeCtMethod() {
+		return "a";
+	}
+
+	@Override
+	public String getPacketInjectorHandleCtMethod() {
+		return "a";
+	}
+
+	@Override
+	public String getLoginNettyInjectHandlerNetworkManager() {
+		return "l";
+	}
+
+}
